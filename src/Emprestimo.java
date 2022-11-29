cfec76716ccfcbcfb1d8bed4754abad94fdcc523
Jose <kaneki1212112121@gmail.com>
2022-11-29 16:04:11 -0300

    Emprestimo

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -1,29 +1,33 @@
 import javax.swing.*;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Emprestimo {
-    public static float valortotal = 0;
     public static void main(String[] args) throws FileNotFoundException {
         StringBuilder impressao = new StringBuilder();
-        Scanner ler = new Scanner(System.in);
-        System.out.println("informe o valor do emprestimo");
-        float valorDoEmprestimo = Float.parseFloat(JOptionPane.showInputDialog(null,"informe o valor do emprestimo"));
-        JOptionPane.showMessageDialog(null,possibilidadesDeParcela(impressao,valorDoEmprestimo, 0.04f,1,12));
-
-        System.out.println("escolha as parcelas de 1 a 12");
-        int parcelas = Integer.parseInt(JOptionPane.showInputDialog("escolha as parcelas de 1 a 12"));
-        JOptionPane.showMessageDialog(null,valorEmprestimoEscolhido(valorDoEmprestimo,0.04f, parcelas));
 
         String id = new String(new SetPaths().GetPaths(new URIpadrao().CacheIdUserName()));
         id = String.valueOf(Cadastro.criptografia(id));
         String caminho = new URIpadrao().URI()+ id +"\\CadastroCliente.txt";
+        float renda = rendaTotal(caminho);
 
+        float valorDoEmprestimo = Float.parseFloat(JOptionPane.showInputDialog(null,"informe o valor do emprestimo"));
+        if (valorDoEmprestimo>=0.30*renda)
+        {
+            valorDoEmprestimo = Float.parseFloat(JOptionPane.showInputDialog(null,"informe o valor do emprestimo"));
+        }
+
+        JOptionPane.showMessageDialog(null,possibilidadesDeParcela(impressao,valorDoEmprestimo, 0.04f,1,12));
+
+        int parcelas = Integer.parseInt(JOptionPane.showInputDialog("escolha as parcelas de 1 a 12"));
+        JOptionPane.showMessageDialog(null,valorEmprestimoEscolhido(valorDoEmprestimo,0.04f, parcelas));
 
     }
 
     public static StringBuilder possibilidadesDeParcela(StringBuilder impressao, float valor, float taxaDeJuros, int parcelaInicial, int parcelaFinal){
-        if (parcelaInicial <= parcelaFinal){
+        if (parcelaInicial <= parcelaFinal)
+        {
             StringBuilder montante = new StringBuilder(String.valueOf((valor * Math.pow((1 + taxaDeJuros), parcelaInicial))/ parcelaInicial));
             impressao.append(parcelaInicial).append(" parcela fica R$");
             impressao.append(montante, 0, montante.indexOf(".")+3);
@@ -36,5 +40,19 @@ public class Emprestimo {
     public static float valorEmprestimoEscolhido(float valor, float taxaDeJuros, int parcela){
         return (float) (valor * Math.pow((1 + taxaDeJuros), parcela));
     }
+    public static float rendaTotal(String caminho) throws FileNotFoundException {
+        float renda = 0;
+        Scanner lerArquivo = new Scanner(new File(caminho));
+        while (lerArquivo.hasNext())
+        {
+            String linha = lerArquivo.nextLine();
+            if (linha.contains("Renda Total: R$"))
+            {
+                renda = Float.parseFloat(linha.substring(linha.indexOf(":")+5));
+            }
+        }
+        return renda;
+    }
+
 
 }
